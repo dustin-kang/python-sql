6ca61ddb978c3049185abebdd0509debb819bc13
Dustin <iamdustin@naver.com>
2022-04-21 18:21:52 +0900

    220421 조건문

--- pythons/baekjoon/조건문.py
+++ pythons/baekjoon/조건문.py
@@ -65,6 +65,34 @@ def alram(H,M):
 alram(H,M)
 
 
+# 2525
+H, M = map(int, input().split())
+timer = int(input()) 
 
+H += timer // 60
+M += timer % 60
 
+if M >= 60:
+    H += 1
+    M -= 60
+if H >= 24:
+    H -= 24
 
+print(H,M)
+
+# 2480
+# 주사위를 3개를 던져서
+# 3개가 같으면 10000 + 같은눈 X 1000 의 상금
+# 2개가 같으면 10000 + 같은눈 X 100 의 상금
+# 모두 다른 눈이면 10000 + 그중 가장 큰 눈 X 100 의 상금
+
+a,b,c = map(int, input().split())
+
+if a == b == c :
+    print(10000 + (a * 1000))
+elif a == b or b == c :
+    print(1000 + (b * 100))
+elif a == c :
+    print(1000 + (a * 100))
+else :
+    print(max(a,b,c) * 100)
