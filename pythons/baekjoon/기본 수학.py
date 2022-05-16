accd4960bb94028653f44ac56f6996c5482f994a
Dustin <iamdustin@naver.com>
2022-05-16 18:12:29 +0900

    220516 기본수학

--- pythons/baekjoon/기본 수학.py
+++ pythons/baekjoon/기본 수학.py
@@ -4,8 +4,11 @@
 2. 벌집 (2292)
 3. 분수 세기(1193)
 4. 달팽이는 올라가고 싶다. (2869)
+5. ACM 호텔 (10250)
+6. 부녀 회장이 될테야 (2775)
+7. 설탕 배달 (2839)
+8. 큰 수 (10757)
 """
-from re import A
 import sys
 
 # # 1712
@@ -81,5 +84,102 @@ import sys
 # print(int(day) if day == int(day) else int(day)+1)
 
 
+# # 10250
+# """
+# ACM 호텔
+# H : 층 수(Heigh)
+# W : 방 수(Width)
+# N : 번째 수 
+# 가장 왼쪽 부터 높이가 낮은 순 우선
+# """
+# num  = int(input())
+# for i in range(num):
+
+#     h,w,n = map(int, sys.stdin.readline().split())
+#     x = n // h + 1 # 호 수
+#     y = n % h # 층 수
+#     if n % h == 0: # 층 수 보다 작은 경우  
+#         x = n // h
+#         y = h
+#     print(f"{y*100+x}")
+
+# # 2775
+# """
+# 거주의 조건
+# 자신의 아래(a-1)층의 1:b 호까지의 사람들의 수의 합 만큼 데려온다는 것을 지켜야한다.
+# k 층 n호에 몇명이 살고 있는지 출력해라
+
+# - 0층부터 있으며 0층 i호에는 i명이 산다는 전제.
+
+# 입력 
+# Test case
+# k
+# n
+
+# 3층 : 1호(1) 2호(1+4=5) 3호(1+4+10=15)
+# 2층 : 1호(1) 2호(1+3=4) 3호(1+3+6=10) 4호(1+3+6+10=20)
+# 1층 : 1호(1) 2호(1+2=3) 3호(1+2+3 =6) 4호(1+2+3+4=10)
+# -----------------------------------------------------
+# 0층 : 1호(1) 2호(2) 3호(3) 4호(4)
+# """
+# T = int(input())
+# s = 0
+# for _ in range(T):
+#     floor = int(input()) 
+#     num = int(input())
+#     f0 = [x for x in range(1, num+1)] # 0층 리스트
+#     for f in range(floor): # 층 만큼 먼저 반복
+#         for i in range(1, num): # 인덱스 조회를 통해 값 추가하기
+#             f0[i] += f0[i-1] # 사람 수 변경
+#     print(f0[-1]) # 가장 마지막 수 출력
 
 
+
+# # 2839
+# """
+# 설탕 배달
+# - 3kg 5kg
+# - 최대한 적은 봉지로 몇개를 가져가면 좋은가.
+# """
+# n = int(input())
+
+# cnt = 0
+# while n >= 0:
+#     if n % 5 == 0:
+#         cnt += (n // 5)
+#         print(cnt)
+#         break
+#     n -= 3
+#     cnt += 1
+# else :
+#     print(-1)
+
+
+# # 10757 <- 여기 부터 시작
+# """
+# 큰수  A+B
+
+#   9223372036854775807
+# + 9223372036854775808
+# -----------------------
+# """
+
+# a, b = map(str, sys.stdin.readline().split())
+# lst = []
+# tmp = 0
+# num = -1
+
+# while num != -len(a)-1: 
+#     some_num = int(a[num]) + int(b[num])
+#     if a[num] == a[0]:
+#         lst.insert(0, f'{some_num}')
+#         break
+#     if some_num // 10 == 0:
+#         lst.insert(0,f'{some_num + tmp}')
+#         tmp = 0
+#     else :
+#         lst.insert(0,f'{some_num % 10 + tmp}')
+#         tmp = some_num // 10
+#     num -= 1
+
+# print(int(''.join(lst)))
