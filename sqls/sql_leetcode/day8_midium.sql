f4853f7ff4c2f56cce543d23201b2c19545d8e3f
Dustin <iamdustin@naver.com>
2022-06-02 15:33:31 +0900

    220602 SQL RANK()

--- sqls/sql_leetcode/day8_midium.sql
+++ sqls/sql_leetcode/day8_midium.sql
@@ -1,7 +1,37 @@
---177.
+--177. Nth Highest Salary
+-- issues 참조
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+RETURN (
+# Write your MySQL query statement below.
+SELECT C.salary
+FROM(
+SELECT salary,
+DENSE_RANK() OVER(ORDER BY salary DESC ) AS 'RANK'
+FROM Employee
+) as C
+WHERE C.RANK = N
+ORDER BY C.salary limit 1
+);
+END
 
+--184. Department Highest Salary
+SELECT
+    D.name AS 'Department',
+    E.name AS 'Employee',
+    E.salary AS 'Salary'
+FROM
+    Employee AS E JOIN
+    Department AS D ON
+    E.departmentId = D.id
+WHERE
+    (E.DepartmentID, Salary) IN
+    ( SELECT
+            DepartmentId, MAX(Salary)
+      FROM
+            Employee
+      GROUP BY DepartmentId
+    )
+;
 
---184.
-
-
---626.
+--626. Exchange Seats
