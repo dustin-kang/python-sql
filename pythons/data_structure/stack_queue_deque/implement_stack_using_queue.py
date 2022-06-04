fa0d85ff23f1afe1db9e16f898d1d0bd2ee57cf2
Dustin <iamdustin@naver.com>
2022-06-04 17:15:31 +0900

    220604 Stack : 큐를 이용한 스택 구현

--- pythons/data_structure/stack_queue_deque/implement_stack_using_queue.py
+++ pythons/data_structure/stack_queue_deque/implement_stack_using_queue.py
@@ -9,7 +9,7 @@
 
 
 - 난이도 : ⭐️
-
+- 시간복잡도: O(n)
 
 
 [EXAMPLE 1]
@@ -28,3 +28,37 @@ myStack.top(); // return 2
 myStack.pop(); // return 2
 myStack.empty(); // return False
 """
+
+from collections import deque
+
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x) #[2-1] + 3
+        
+        # 두번째 부터 요소 삽입 후 방금 삽입 한 요소를 맨 앞에 두는 상태로 정렬
+        for _ in range(len(self.q) - 1): # 2번 진행
+            self.q.append(self.q.popleft()) # [2-1-3] -> [1-3-2] -> [3-2-1]
+
+    def pop(self) -> int:
+        return self.q.popleft()
+        
+
+    def top(self) -> int:
+        return self.q[0]
+        
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+
+myStack = MyStack()
+myStack.push(1)
+myStack.push(2)
+myStack.push(3)
+myStack.top()
+myStack.pop()
+myStack.empty()
