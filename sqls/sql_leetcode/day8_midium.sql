24b02be3fb4fdfe763f88fe9a5671d6b1978e02f
dustin <dustin@dustinui-MacBookPro.local>
2022-06-13 15:46:33 +0900

    220613 SQL Exchanger odd , even id student

--- sqls/sql_leetcode/day8_midium.sql
+++ sqls/sql_leetcode/day8_midium.sql
@@ -35,3 +35,34 @@ WHERE
 ;
 
 --626. Exchange Seats
+-- 짝수 학생과 홀수 학생의 데이터 교체
+SELECT id,
+CASE 
+    WHEN id % 2 = 0 THEN (SELECT student FROM Seat where id = (s.id-1))
+    -- 짝수인 경우 id - 1 인 학생으로
+    WHEN id % 2 != 0 AND id < (SELECT COUNT(student) from Seat) THEN (SELECT student FROM seat WHERE id = (s.id+1))
+    -- 홀수인 경우 와 총 학생수보다 작은 경우는 id + 1 인 학생으로
+    ELSE student
+    -- 그외(마지막)인 경우는 그대로
+END AS student
+FROM Seat AS s
+
+
+SELECT
+	CASE
+		WHEN seat.id % 2 != 0 AND seat.id = (SELECT COUNT(*) FROM seat) THEN seat.id -- 마지막 학생
+		WHEN seat.id % 2 = 0 THEN seat.id - 1 -- 짝수 학생
+		ELSE
+			seat.id + 1 -- 홀수 학생
+	END as id,
+	student 
+FROM seat
+ORDER BY id
+;
+
+SELECT
+IF (id < (SELECT COUNT(*) FROM Seat), IF (id MOD 2 = 0, id-1, id+1), if(id MOD 2 = 0, id-1, id)) AS id, student
+FROM Seat
+ORDER BY id ASC;
+
+-- MODE(200, 7) => 4 나머지를 구하는 연산
