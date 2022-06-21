afede7fd52b61df3b0fd85af4f1953a727fcabb3
dustin <dustin@dustinui-MacBookPro.local>
2022-06-21 14:18:02 +0900

    220621 Leetcode SQL Hard2

--- sqls/sql_leetcode/day10_hard2.sql
+++ sqls/sql_leetcode/day10_hard2.sql
@@ -1,3 +1,13 @@
 -- 262. Trips and Users
 
-https://leetcode.com/problems/trips-and-users/ 
+SELECT
+    Trips.Request_at AS Day
+    ROUND(SUM(IF(status != 'complete', 1, 0)) / count(*), 2) AS 'Cancellation Rate'
+FROM
+    Trips, Users
+WHERE
+    Trips.Client_Id = Users.Users_id AND
+    Users.Banned = "No" AND
+    Trips.Request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY
+    Trips.Request_at
